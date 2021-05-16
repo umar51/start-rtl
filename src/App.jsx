8ca99800d42e092cfc806b634c01e46dc6bb9739
umar51 <66181738+umar51@users.noreply.github.com>
2021-05-16 23:54:36 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Getting started with React testing library
+        </h2>
+      </header>
+      
+    
+    </div>
+  );
+}
+
+export default App;
